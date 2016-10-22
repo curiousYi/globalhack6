b1e3d9310708d275cd58242dea281631c0fad928
rheemansum <rheewalt@gmail.com>
2016-10-22 16:43:12 -0500

    filled

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ allClientData.forEach(client => {
     // console.log(client);
   }
 })
-// console.log(clientObj);
+console.log(clientObj);
 
 // reading data
 pdfFillForm.read('ss-5.pdf')
 .then(res => {
   // console.log(res);
   res.forEach(field => {
-    console.log(field.name);
+    // console.log(field.name);
     // for each field in the pdf form, look in the database to see if there's relevant info
     if(field.name.toLowerCase().search(/(?=.*mother)(?=.*first)(?=.*name)/) > -1){
       if(clientObj.Mother_First_Name){
@@ -141,13 +141,27 @@ pdfFillForm.read('ss-5.pdf')
       pdfFillObj[field.name] = clientObj.SSN.slice(5,9)
 
     }
-    else if(clientObj.SSNDataQuality === 1 && field.name.toLowerCase().search(/(?=.*TextFieldName18\[0\])/) > -1){
-      pdfFillObj[field.name] = 'RTZYPI'
+    // else if(clientObj.SSNDataQuality === 1 && field.name.toLowerCase().search(/(?=.*TextFieldName18\[0\])/) > -1){
+    //   pdfFillObj[field.name] = 'RTZYPI'
 
-    }
+    // }
     else if(field.name.toLowerCase().search(/(?=.*gender\[0\])/) > -1){
-      pdfFillObj[field.name] = true;
+      if(clientObj.Gender === 1){
+        pdfFillObj[field.name] = true;
+      }
     }
+    else if(field.name.toLowerCase().search(/(?=.*gender\[1\])/) > -1){
+      if(clientObj.Gender === 0){
+        pdfFillObj[field.name] = true;
+      }
+    }
+    else if(field.name.toLowerCase().search(/(?=.*state)(?=.*birth)/) > -1){
+      if(clientObj.Birth_State){
+        pdfFillObj[field.name] = clientObj.Birth_State
+      }
+    }
+
+
 
     // console.log(pdfFillObj);
 
