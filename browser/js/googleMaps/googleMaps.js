496ff221bc7fc226526a3b10e1ca02ce864e24c7
ychao21 <changingtimes@gmail.com>
2016-10-23 06:07:14 -0500

    Finished adding job location markers and hoverbox event listeners for location markers on google maps

--- browser/js/googleMaps/googleMaps.js
+++ browser/js/googleMaps/googleMaps.js
@@ -15,6 +15,9 @@ app.config(function ($stateProvider) {
 
 
 app.controller( 'jobsMapCtrl', function($scope, jobLocsFactory, jobs){
+    var idCounter = 0;
+    $scope.infoWindowArrays = [];
+
      angular.extend($scope, {
         map: {
             center: {
@@ -24,36 +27,82 @@ app.controller( 'jobsMapCtrl', function($scope, jobLocsFactory, jobs){
             zoom: 12,
             markers: [],
             events: {
-                click: function (map, eventName, originalEventArgs) {
-                    var e = originalEventArgs[0];
-                    var lat = e.latLng.lat(),lon = e.latLng.lng();
-                    var marker = {
-                        id: Date.now(),
-                        coords: {
-                            latitude: lat,
-                            longitude: lon
-                        },
-                    };
-                    $scope.map.markers.push(marker);
+                click: function (map, eventName, model, originalEventArgs, $scope) {
+                    // console.log('hey there');
+                    // console.log('Marker was clicked' + map)
+                    //                     console.log('eventName' + eventName)
+                    //                     console.log('model' + model)
+                    //                     console.log('originalEventArgs' + originalEventArgs)
+
+                    // var e = originalEventArgs[0];
+                    // var lat = e.latLng.lat(),lon = e.latLng.lng();
+                    // var marker = {
+                    //     id: Date.now(),
+                    //     coords: {
+                    //         latitude: lat,
+                    //         longitude: lon
+                    //     },
+                    // };
+                    // $scope.map.markers.push(marker);
                 }
             }
         }
     });
 
-    markerInit(jobs);
+    markerInit(jobs, idCounter, $scope);
 
-    function markerInit (jobs) {
+    function markerInit (jobs, idCounter, $scope) {
+        console.log('heres the $scope', $scope);
+        var infoWindowsArrays = $scope.infoWindowArrays;
+
         jobs.forEach(job => {
+            var newInfoWindow = new google.maps.InfoWindow({
+                content: '<div id="content"> <b>'+
+                'Name of Employer: </b>' + job.employer +
+            '</div>'+'<div ><b>'+
+                'Description: </b>' + job.description +
+            '</div>'+'<div > <b>'+
+                'Industry: </b>' + job.industry +
+            '</div>'
+            ,
+                maxWidth: 200
+            })
+            // console.log('heres the var infoWindowsArrays ', infoWindowsArrays)
+            infoWindowsArrays.push(newInfoWindow)
+
             var marker = {
-                id: Date.now(),
+                id: idCounter,
                 coords: {
                     latitude: job.latitude,
                     longitude: job.longitude
                 },
+                markerEvents: {
+                    click: function (marker, eventName, model, originalEventArgs) {
+                        // console.log('hey there');
+                        console.log('heres infoWindowArrays ,', infoWindowsArrays[model.idKey])
+                        infoWindowsArrays[model.idKey].open(model.map,marker);
+                        console.log('Marker was clicked', marker)
+                        console.log('heres this ', this)
+                        console.log('eventName', eventName)
+                        console.log('model', model)
+                        console.log('originalEventArgs', originalEventArgs)
+                        console.log('heres the var infoWindowsArrays click event in marker', infoWindowsArrays)
+                    }
+                },
+                // infowindow: new google.maps.InfoWindow({
+                //     content: 'Hey there',
+                //     maxWidth: 200
+                // })
             };
             $scope.map.markers.push(marker);
+            idCounter++;
         })
     }
+
+    $scope.seeInfo = function(marker){
+        console.log('here is the marker you clicked', marker)
+    }
+
 })
 
 app.factory('jobLocsFactory', function($http){
